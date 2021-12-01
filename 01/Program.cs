712f2d51ff72d278051a7171e32b8e32b3faafe8
Jonas Kubesch <jkubesch.mmt-b2020@fh-salzburg.ac.at>
2021-12-01 08:38:47 +0100

    Task1 and Structure

---
+++ 01/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace _01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader sr = new StreamReader("input.txt"))
+            {
+                string[] input = sr.ReadToEnd().Split("\r\n");
+
+                int currentVal, previousVal;
+
+                int succesCouter = 0;
+                int failCounter = 0;
+
+                for (var i = 1; i < input.Length - 2; i++)
+                {
+                    previousVal = int.Parse(input[i - 1]) + int.Parse(input[i]) + int.Parse(input[i + 1]) ;
+                    currentVal = int.Parse(input[i]) + int.Parse(input[i + 1]) + int.Parse(input[i + 2]);
+
+                    if (previousVal < currentVal)
+                    {
+                        succesCouter++;
+                    }
+                    else
+                    {
+                        failCounter++;
+                    }
+                }
+
+                Console.WriteLine("Sucess: " + succesCouter + "\nFail: " + failCounter);
+            }
+        }
+    }
+}
---
+++ SampleCode/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace _01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader sr = new StreamReader("input.txt"))
+            {
+                string[] input = sr.ReadToEnd().Split("\r\n");
+
+                for (var inputIndex = 1; inputIndex < input.Length; inputIndex++)
+                {
+
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
