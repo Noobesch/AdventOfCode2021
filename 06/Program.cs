695b81b5dc8d0013585a57c5ce7043805ab8e692
Jonas Kubesch <jkubesch.mmt-b2020@fh-salzburg.ac.at>
2021-12-06 08:01:56 +0100

    Add Day6_2

--- 06/Program.cs
+++ 06/Program.cs
@@ -14,15 +14,14 @@ namespace _06
                 List<long> input = new List<long>();
                 List<long> inputAfterDay = new List<long>();
 
-
                 input.AddRange(sr.ReadToEnd().Split(",").Select(long.Parse).ToArray());
 
 
                 for (var day = 0; day < 80; day++)
                 {
-                    foreach(var timer in input)
+                    foreach (var timer in input)
                     {
-                        if(timer == 0)
+                        if (timer == 0)
                         {
                             inputAfterDay.Add(6);
                             inputAfterDay.Add(8);
@@ -39,6 +38,56 @@ namespace _06
                 }
 
                 Console.WriteLine($"After 80 days there are {input.Count} fish");
+
+            }
+            //Task B
+
+            using (StreamReader sr = new StreamReader("input.txt"))
+            {
+
+                long[] amountArray = new long[9];
+
+                long[] inputNumbers = sr.ReadToEnd().Split(",").Select(long.Parse).ToArray();
+
+                //Init array
+                foreach (var number in inputNumbers)
+                {
+                    amountArray[number]++;
+                }
+
+                for (var day = 0; day < 256; day++)
+                {
+                    long[] amountArrayAfter = new long[9];
+
+                    for (var amountIndex = 0; amountIndex < amountArray.Length; amountIndex++)
+                    {
+                        long amount = amountArray[amountIndex];
+
+                        if (amountIndex == 0)
+                        {
+                            amountArrayAfter[6] = amount;
+                            amountArrayAfter[8] = amount;
+                        }
+                        else
+                        {
+                            amountArrayAfter[amountIndex - 1] += amount;
+                        }
+                    }
+
+                    for (var amountIndex = 0; amountIndex < amountArray.Length; amountIndex++)
+                    {
+                        amountArray[amountIndex] = amountArrayAfter[amountIndex];
+                    }
+                }
+
+                long result = 0;
+
+                foreach(var value in amountArray)
+                {
+                    result += value;
+                }
+
+                Console.WriteLine($"After 256 days there are {result} fish");
             }
         }
     }
