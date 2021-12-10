85e317ac71548a6dcae63c27c08915aaa6d8c6ec
Jonas Kubesch <jkubesch.mmt-b2020@fh-salzburg.ac.at>
2021-12-10 09:32:58 +0100

    Day 10 both Task

---
+++ 07/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader sr = new StreamReader("input.txt"))
+            {
+                List<long> inputs = sr.ReadToEnd().Split(",").Select(long.Parse).ToList();
+
+                inputs.Sort();
+
+                long min = inputs.Min();
+                long max = inputs.Max();
+
+                long[] fuelArray = new long[max - min];
+
+                long minFuelIndex = long.MaxValue;
+                long minFuelUsed = long.MaxValue;
+
+                for (var inputIndex = min; inputIndex < max; inputIndex++)
+                {
+                    foreach(var input in inputs)
+                    {
+                        long distance = Math.Abs(input - inputIndex);
+                        fuelArray[inputIndex] += distance * (distance + 1) / 2;
+                    }
+
+                    if(fuelArray[inputIndex] < minFuelUsed)
+                    {
+                        minFuelUsed = fuelArray[inputIndex];
+                        minFuelIndex = inputIndex;
+                    }
+                }
+
+                Console.WriteLine($"Least fuel used is {minFuelIndex + min} with a use of {minFuelUsed}");
+            }
+        }
+    }
+}
---
+++ 10/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace _10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader sr = new StreamReader("input.txt"))
+            {
+                string[] inputLines = sr.ReadToEnd().Split("\r\n");
+                List<char[]> lineValues = new List<char[]>();
+
+                foreach (var line in inputLines)
+                {
+                    lineValues.Add(line.ToCharArray());
+                }
+
+                long resultTaskA = 0;
+
+                List<long> taskBResults = new List<long>();
+                long resultTaskB = 0;
+
+                foreach (var line in lineValues)
+                {
+                    long currentValueTaskA = 0;
+                    long currentValueTaskB = 0;
+                    Stack<char> characterStack = new Stack<char>();
+
+                    foreach (var character in line)
+                    {
+                        switch (character)
+                        {
+                            case '(':
+                            case '[':
+                            case '{':
+                            case '<':
+                                characterStack.Push(character);
+                                break;
+
+                            case ')':
+                                currentValueTaskA += ('(' == characterStack.Pop()) ? 0 : 3;
+                                break;
+
+                            case ']':
+                                currentValueTaskA += ('[' == characterStack.Pop()) ? 0 : 57;
+                                break;
+
+                            case '}':
+                                currentValueTaskA += ('{' == characterStack.Pop()) ? 0 : 1197;
+                                break;
+
+                            case '>':
+                                currentValueTaskA += ('<' == characterStack.Pop()) ? 0 : 25137;
+                                break;
+                        }
+
+                        if (currentValueTaskA != 0)
+                        {
+                            resultTaskA += currentValueTaskA;
+                            break;
+                        }
+                    }
+
+                    if (currentValueTaskA == 0)
+                    {
+                        while (characterStack.Count != 0)
+                        {
+                            switch (characterStack.Pop())
+                            {
+                                case '(':
+                                    currentValueTaskB *= 5;
+                                    currentValueTaskB += 1;
+                                    break;
+
+                                case '[':
+                                    currentValueTaskB *= 5;
+                                    currentValueTaskB += 2;
+                                    break;
+
+                                case '{':
+                                    currentValueTaskB *= 5;
+                                    currentValueTaskB += 3;
+                                    break;
+
+                                case '<':
+                                    currentValueTaskB *= 5;
+                                    currentValueTaskB += 4;
+                                    break;
+                            }
+                        }
+
+                        taskBResults.Add(currentValueTaskB);
+                    }
+                }
+
+                taskBResults.Sort();
+                resultTaskB = taskBResults[taskBResults.Count / 2];
+
+                Console.WriteLine(resultTaskA);
+                Console.WriteLine(resultTaskB);
+            }
+        }
+    }
+}
